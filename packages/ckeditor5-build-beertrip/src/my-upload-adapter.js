3bdd16599b00399b21affb4d16d8c2008fcad3d6
volteris <damien.blaise@smartfizz.fr>
2021-02-24 11:56:23 +0100

    beertrip

---
+++ packages/ckeditor5-build-beertrip/src/my-upload-adapter.js
@@ -0,0 +1,190 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import Compressor from 'compressorjs';
+// import { logWarning } from '../../ckeditor5-utils/src';
+// import FileRepository from "../../ckeditor5-upload/src/filerepository";
+
+export default class BeertripUploadAdapter extends Plugin {
+	/**
+	 * @inheritDoc
+	 */
+	/*
+	static get requires() {
+		return [ FileRepository ];
+	}
+	 */
+
+	/**
+	 * @inheritDoc
+	 */
+	static get pluginName() {
+		return 'BeertripUploadAdapter';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	init() {
+		const options = this.editor.config.get( 'beertripUpload' );
+
+		if ( !options ) {
+			return;
+		}
+
+		if ( !options.uploadUrl ) {
+			/**
+			 * The {@link module:upload/adapters/simpleuploadadapter~SimpleUploadConfig#uploadUrl `config.simpleUpload.uploadUrl`}
+			 * configuration required by the {@link module:upload/adapters/simpleuploadadapter~SimpleUploadAdapter `SimpleUploadAdapter`}
+			 * is missing. Make sure the correct URL is specified for the image upload to work properly.
+			 *
+			 * @error simple-upload-adapter-missing-uploadurl
+			 */
+			console.log( 'beertrip-upload-adapter-missing-uploadurl' );
+
+			return;
+		}
+
+		this.editor.plugins.get( 'FileRepository' ).createUploadAdapter = loader => {
+			return new MyUploadAdapter( loader, options );
+		};
+	}
+}
+
+class MyUploadAdapter {
+	/**
+	 * Creates a new adapter instance.
+	 *
+	 * @param {module:upload/filerepository~FileLoader} loader
+	 * @param {module:upload/adapters/simpleuploadadapter~SimpleUploadConfig} options
+	 */
+	constructor( loader, options ) {
+		/**
+		 * FileLoader instance to use during the upload.
+		 *
+		 * @member {module:upload/filerepository~FileLoader} #loader
+		 */
+		this.loader = loader;
+
+		/**
+		 * The configuration of the adapter.
+		 *
+		 * @member {module:upload/adapters/simpleuploadadapter~SimpleUploadConfig} #options
+		 */
+		this.options = options;
+	}
+
+	/**
+	 * Starts the upload process.
+	 *
+	 * @see module:upload/filerepository~UploadAdapter#upload
+	 * @returns {Promise}
+	 */
+	upload() {
+		return this.loader.file
+			.then( file => new Promise( ( resolve, reject ) => {
+				this._initRequest();
+				this._initListeners( resolve, reject, file );
+				this._sendRequest( file );
+			} ) );
+	}
+
+	/**
+	 * Aborts the upload process.
+	 *
+	 * @see module:upload/filerepository~UploadAdapter#abort
+	 * @returns {Promise}
+	 */
+	abort() {
+		if ( this.xhr ) {
+			this.xhr.abort();
+		}
+	}
+
+	/**
+	 * Initializes the `XMLHttpRequest` object using the URL specified as
+	 * {@link module:upload/adapters/simpleuploadadapter~SimpleUploadConfig#uploadUrl `simpleUpload.uploadUrl`} in the editor's
+	 * configuration.
+	 *
+	 * @private
+	 */
+	_initRequest() {
+		const xhr = this.xhr = new XMLHttpRequest();
+
+		xhr.open( 'POST', this.options.uploadUrl, true );
+		xhr.responseType = 'json';
+	}
+
+	/**
+	 * Initializes XMLHttpRequest listeners
+	 *
+	 * @private
+	 * @param {Function} resolve Callback function to be called when the request is successful.
+	 * @param {Function} reject Callback function to be called when the request cannot be completed.
+	 * @param {File} file Native File object.
+	 */
+	_initListeners( resolve, reject, file ) {
+		const xhr = this.xhr;
+		const loader = this.loader;
+		const genericErrorText = `Couldn't upload file: ${ file.name }.`;
+
+		xhr.addEventListener( 'error', () => reject( genericErrorText ) );
+		xhr.addEventListener( 'abort', () => reject() );
+		xhr.addEventListener( 'load', () => {
+			const response = xhr.response;
+
+			if ( !response || response.error ) {
+				return reject( response && response.error && response.error.message ? response.error.message : genericErrorText );
+			}
+
+			resolve( response.url ? { default: response.url } : response.urls );
+		} );
+
+		// Upload progress when it is supported.
+		/* istanbul ignore else */
+		if ( xhr.upload ) {
+			xhr.upload.addEventListener( 'progress', evt => {
+				if ( evt.lengthComputable ) {
+					loader.uploadTotal = evt.total;
+					loader.uploaded = evt.loaded;
+				}
+			} );
+		}
+	}
+
+	/**
+	 * Prepares the data and sends the request.
+	 *
+	 * @private
+	 * @param {File} file File instance to be uploaded.
+	 */
+	_sendRequest( file ) {
+		// Set headers if specified.
+		const headers = this.options.headers || {};
+
+		// Use the withCredentials flag if specified.
+		const withCredentials = this.options.withCredentials || false;
+
+		for ( const headerName of Object.keys( headers ) ) {
+			this.xhr.setRequestHeader( headerName, headers[ headerName ] );
+		}
+
+		this.xhr.withCredentials = withCredentials;
+
+		const cxhr = this.xhr;
+		new Compressor( file, {
+			convertSize: 1000000, // MaxSize 1MB
+			success( result ) {
+				// Prepare the form data.
+				const data = new FormData();
+
+				data.append( 'upload', result );
+
+				// Send the request.
+				cxhr.send( data );
+			},
+			error( error ) {
+				console.log( error );
+			}
+		} );
+	}
+}
+
